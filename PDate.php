2d57d777debca176d00d942c12157cd0859309ef
Mohammad <mds1121@yahoo.com>
2018-10-14 17:58:27 +0330

    Update PDate.php
    add `inTimeZone`  in `$config` parameters, so we can set the timezone of DateTime we want to convert.
    change `timezone` to to `outTimezone`.

--- PDate.php
+++ PDate.php
@@ -3,19 +3,25 @@ class PDate
 {
     private $config =
         [
-        'dateTime'  => null,
-        'y'         => null,
-        'm'         => null,
-        'd'         => null,
-        'h'         => 0,
-        'i'         => 0,
-        's'         => 0,
-        'inFormat'  => 'Y-m-d',
-        'outFormat' => 'y-M-d',
-        'local'     => null,
-        'calendar'  => 'persian',
-        'timeZone'  => null,
+        'dateTime'    => null,
+        'y'           => null,
+        'm'           => null,
+        'd'           => null,
+        'h'           => 0,
+        'i'           => 0,
+        's'           => 0,
+        'inFormat'    => 'Y-m-d',
+        'outFormat'   => 'y-M-d',
+        'local'       => null,
+        'calendar'    => 'persian',
+        'outTimeZone'  => null,
     ];
+
+    public function __construct()
+    {
+        $this->config['inTimeZone'] = date_default_timezone_get();
+
+    }
 
     public function setConfig($config)
     {
@@ -25,12 +31,7 @@ class PDate
     public function checkDate($config, $baseCalType)
     {
         if (isset($config['dateTime'])) {
-            if ($config['dateTime'] === 'now') {
-                $time = new DateTime();
-                $time->format($config['inFormat']);
-            } else {
-                $time = DateTime::createFromFormat($config['inFormat'], $config['dateTime']);
-            }
+            $time = DateTime::createFromFormat($config['inFormat'], $config['dateTime'], new DateTimeZone($config['inTimeZone']));
         } else {
             if (empty($config['y']) || empty($config['m']) || empty($config['d'])) {
                 throw new Exception("y, m and d are required parameters");
@@ -38,7 +39,7 @@ class PDate
             //months num starts from 0
             $config['m'] = $config['m'] - 1;
 
-            $time = IntlCalendar::createInstance($config['timeZone'], "$config[local]@calendar=$baseCalType");
+            $time = IntlCalendar::createInstance($config['inTimeZone'], "$config[local]@calendar=$baseCalType");
 
             $time->set($config['y'], $config['m'], $config['d'], $config['h'], $config['i'], $config['s']);
         }
@@ -52,7 +53,7 @@ class PDate
         $config = array_merge($this->config, $config);
         $time   = $this->checkDate($config, 'gregorian');
 
-        $formatter = new IntlDateFormatter($config['local'] . "@calendar={$config['calendar']}", IntlDateFormatter::SHORT, IntlDateFormatter::LONG, $config['timeZone'], IntlDateFormatter::TRADITIONAL, $config['outFormat']);
+        $formatter = new IntlDateFormatter($config['local'] . "@calendar={$config['calendar']}", IntlDateFormatter::SHORT, IntlDateFormatter::LONG, $config['outTimeZone'], IntlDateFormatter::TRADITIONAL, $config['outFormat']);
 
         // $formatter->setPattern($config['outFormat']);
         $time = $formatter->format($time);
@@ -64,7 +65,7 @@ class PDate
         $config = array_merge($this->config, $config);
         $time   = $this->checkDate($config, 'persian');
 
-        $formatter = new IntlDateFormatter($config['local'] . "@calendar={$config['calendar']}", IntlDateFormatter::FULL, IntlDateFormatter::FULL, $config['timeZone'], IntlDateFormatter::GREGORIAN, $config['outFormat']);
+        $formatter = new IntlDateFormatter($config['local'] . "@calendar={$config['calendar']}", IntlDateFormatter::FULL, IntlDateFormatter::FULL, $config['outTimeZone'], IntlDateFormatter::GREGORIAN, $config['outFormat']);
 
         //$formatter->setPattern($config['outFormat']);
         $time = $formatter->format($time);
@@ -75,7 +76,9 @@ class PDate
     public function now($config = [])
     {
         $config = array_merge($this->config, $config);
-        $time = IntlCalendar::createInstance($config['timeZone'], 'fa_IR');
+
+        $time = IntlCalendar::createInstance($config['outTimeZone'], 'fa_IR');
+
         $time = IntlDateFormatter::formatObject($time, $config['outFormat'], $config['local']);
 
         return $time;
